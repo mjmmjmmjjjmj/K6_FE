fee584fdb0bcd58efbce5ce0c5e70600d0c9afc6
mjmmjmmjjjmj <kmjtalk77@gmail.com>
2024-02-22 12:59:19 +0900

    javaFunction

--- 03/03.js
+++ 03/03.js
@@ -1,9 +1,18 @@
-// alert("안녕하세요~")
-function handleClick(n) {
+/*
+함수작성 2 : 화살표 함수
+상수로 함수를 정의할 수 있음. 원래 상수에는 값만 넣는데...
+
+*/
+const handleClick = (n) => {
+    document.querySelector ("#msgArea").innerHTML = `<h2>button ${n}이 눌러졌습니다</h2>`;
+}
+/*함수작성 1 */
+/* alert("안녕하세요~")
+// function handleClick(n) {
     //메세지 영역 가져오기 1
     //document.getElementById("msgArea").innerHTML = "안녕하세요";\
     
     //메세지 영역 가져오기 2
     //document.querySelector ("#msgArea").innerHTML = "<h2>button "+ n +"이 눌러졌습니다</h2>";
-    document.querySelector ("#msgArea").innerHTML = `<h2>button ${n}이 눌러졌습니다</h2>`;
-}
+    // document.querySelector ("#msgArea").innerHTML = `<h2>button ${n}이 눌러졌습니다</h2>`;
+}*/
